aa4e922d5d5362b382e320ac0108596fb6aa45e1
kkleindev <kleinkevin.lu@gmail.com>
2017-10-11 23:13:40 +0200

    Adhere code to Google's style guide. In particular: - define global parameters - use underscore notation for variables - replace hard tabs by spaces - make variable names explicit

--- 1-lsh/src/diggers.py
+++ 1-lsh/src/diggers.py
@@ -1,36 +1,34 @@
-import numpy as np
 import sys
+import numpy as np
+
+# The number of bands in signature matrix.
+N_BANDS = 20
+# The number of rows per band.
+N_ROWS_PER_BAND = 50
+# A large prime larger than the number of rows in the signature matrix, used for hashing.
+PRIME = 3373
+# Seed used for randomization in every instance of mapper.
+SEED = 1337
 
 def mapper(key, value):
     # key: None
     # value: one line of input file
-
     # initialize int-document array and id
-    # r - number of rows per band
-    # b - number of bands
     shingles = value.split(' ')
-    id = int (shingles[0].split('_')[1])
+    document_id = int(shingles[0].split('_')[1])
     shingles = map(int, shingles[1:])
-    r = 50  
-    b = 20
-    nrows = r*b
-    np.random.seed(seed=1337)
-    sigvec = np.ones(nrows)*sys.maxint
+    n_rows = N_BANDS * N_ROWS_PER_BAND
+    np.random.seed(seed=SEED)
+    sigvec = np.ones(n_rows) * sys.maxint
 
-    # kevins fault
-    hf_vec = map(lambda x:gen_hashfunc(nrows), range(nrows))
+    hash_functions = map(lambda x: gen_hashfunc(n_rows), range(n_rows))
 
-    for j in range(nrows):
-    	for i in range(len(shingles)):
-    		if shingles[i] != 0:
-    			sigvec[j] = np.minimum(hf_vec[j](shingles[i]), sigvec[j])
-
-
+    for j in range(n_rows):
+        for i in range(len(shingles)):
+            sigvec[j] = np.minimum(hash_functions[j](shingles[i]), sigvec[j])
     print(sigvec)
-
     if False:
         yield "key", "value"  # this is how you yield a key, value pair
-
 
 def reducer(key, values):
     # key: key from mapper used to aggregate
@@ -38,16 +36,13 @@ def reducer(key, values):
     if False:
         yield "key", "value"  # this is how you yield a key, value pair
 
-
-def gen_hashfunc(nrows):
-	a = np.random.randint(1,nrows)
-	b = np.random.randint(0,nrows)
-	p = 3373 # large prime > nrows
-	return lambda s:((np.multiply(a,s)+b)%p)%nrows
+def gen_hashfunc(n_rows):
+    a = np.random.randint(1, n_rows)
+    b = np.random.randint(0, n_rows)
+    return lambda s: ((np.multiply(a, s) + b) % PRIME) % n_rows
 
 def min_hash():
-	return 0 
+    return 0
 
 def band_hash():
-	return 0
-
+    return 0
