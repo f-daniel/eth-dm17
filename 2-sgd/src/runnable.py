f98e56fac7b53a03d81279e9f3c0a19f129633d4
kkleindev <kleinkevin.lu@gmail.com>
2017-11-03 16:47:01 +0100

    Turn mapper input into numpy matrices.

--- 2-sgd/src/runnable.py
+++ 2-sgd/src/runnable.py
@@ -8,11 +8,17 @@ def transform(X):
 def mapper(key, value):
     # key: None
     # value: one line of input file
-    yield "key", "value"  # This is how you yield a key, value pair
+    value = map(lambda x: map(float, x.split(" ")), value)
+    # x has datapoints in rows and features in columns. Shape: (2000, 400).
+    x = np.array(map(lambda x: x[1:], value))
+    y = np.array(map(lambda x: x[0], value))
+    # The yielded key should always be the same.
+    #yield "key", "value"  # This is how you yield a key, value pair
 
 
 def reducer(key, values):
+    a = 5
     # key: key from mapper used to aggregate
     # values: list of all value for that key
     # Note that we do *not* output a (key, value) pair here.
-    yield np.random.randn(400)
+    #yield np.random.randn(400)
