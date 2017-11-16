96948cff85b90e5f2bde358b1a2f1e0bb6fce250
kkleindev <kleinkevin.lu@gmail.com>
2017-11-16 13:33:35 +0100

    Remove duplicate features from transformation.

--- 2-sgd/src/runnable.py
+++ 2-sgd/src/runnable.py
@@ -1,19 +1,33 @@
 from __future__ import division
 import numpy as np
+import os
+import psutil
 import random
 
 N_FEATURES = 250
-N_DIM = N_FEATURES + N_FEATURES * N_FEATURES
+N_DIM = int(N_FEATURES + (N_FEATURES  * (N_FEATURES + 1)) / 2)
 N_DATA = 2000
 
 def random_quadratic_transform(x):
-    random.seed(71)
-    # Choose 128 random feature indexes.
-    feature_indexes = map(lambda x: random.randint(0, 399), range(N_FEATURES))
-    features = np.take(x, feature_indexes)
-    # features = x
-    for i in range(N_FEATURES):
-        features = np.concatenate((features, features[i] * features[0:N_FEATURES]), axis = 0)
+    # Include dimension 1 features.
+    ## OPTION1: Random feature subsets.
+    #random.seed(2)
+    ## Choose N_FEATURES many random feature indexes.
+    #feature_indexes = map(lambda x: random.randint(0, 399), range(N_FEATURES))
+    #features = np.take(x, feature_indexes)
+    ## OPTION2: Use full input feature vector.
+    features = x
+
+    # Include squares of each dimension.
+    features = np.concatenate((features, features * features))
+    # Include sqrt(2)*x_i*x_j for i != j.
+    for i in range(N_FEATURES-1):
+        features = np.concatenate((features, np.sqrt(2) * features[i] * features[i+1:N_FEATURES]), axis = 0)
+    pid = os.getpid()
+    py = psutil.Process(pid)
+    memoryUse = py.memory_info()[0]/2.**30
+    if memoryUse > 0.8:
+        print('memory use:', memoryUse)
     return features
 
 def transform(x):
@@ -22,6 +36,7 @@ def transform(x):
         x_t = np.apply_along_axis(random_quadratic_transform, 1, x)
     # 1D transformation.
     else:
+        # print '1d'
         x_t = random_quadratic_transform(x)
     return x_t
 
@@ -34,21 +49,25 @@ def mapper(key, value):
     second_moment = np.zeros(N_DIM)
     learning_rate = 0.001
     weight = np.random.rand(N_DIM)
+    # weight = np.zeros(N_DIM)
     # Normalize weight's norm to be smaller or equal 1.
     weight = weight / np.linalg.norm(weight)
     convergence_threshold = 0.000001
     relative_change = 1
     t = 0
     epsilon = 1e-8
-    n_iterations = 4000
+    n_iterations = 16000
     np.random.shuffle(value)
     # Convert sample strings to lists of floats.
     value = map(lambda sample: map(float, sample.split(" ")), value)
+    ys = np.array([value[i][0] for i in range(N_DATA)])
+    xs = np.array([value[i][1:] for i in range(N_DATA)])
+    xs = transform(xs)
     # Adam.
     while relative_change >= convergence_threshold and t < n_iterations:
-        y = np.array(value[t % N_DATA][0])
-        x = np.array(value[t % N_DATA][1:])
-        x = transform(x)
+        y = ys[t % N_DATA]
+        # x = transform(xs[t % N_DATA])
+        x = xs[t % N_DATA]
         t = t + 1
         # Analytic derivation via case distinction.
         if y * np.dot(weight, x) > 1:
@@ -65,6 +84,10 @@ def mapper(key, value):
                 np.sqrt(corrected_second_moment) + epsilon)
         relative_change = np.linalg.norm(weight - weight_previous) / np.linalg.norm(weight_previous)
     # The yielded key should always be the same.
+    pid = os.getpid()
+    py = psutil.Process(pid)
+    memoryUse = py.memory_info()[0]/2.**30  # memory use in GB...I think
+    print('memory use:', memoryUse)
     print t
     yield "key", np.transpose(weight)  # This is how you yield a key, value pair
 
