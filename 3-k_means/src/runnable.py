27f54456ff32f6df32f51c101a0b074c242fb527
kkleindev <kleinkevin.lu@gmail.com>
2017-11-30 14:43:54 +0100

    Rewrite clustering algorithms from scratch. - Use D^2 sampling for initial center positions ('kmeans++') - Apply offline kmeans algorithm on a u.a.r. drawn subset of the data. Note that this version beats the project's hard baseline.

--- 3-k_means/src/runnable.py
+++ 3-k_means/src/runnable.py
@@ -1,39 +1,54 @@
+from __future__ import division
+
 import numpy as np
+from numpy.random import randint, choice, shuffle
+from numpy.linalg import norm
 import sys
 
-N_CENTERS = 200
-# The number of times the k-means algorithms passes over the whole dataset at hand.
-N_PASSES = 4
-
-def k_means(data, n_iterations):
-    # Center initialization: 200 random points from dataset.
-    np.random.shuffle(data)
-    centers = data[:N_CENTERS,:]
-    np.random.shuffle(data)
-    # n_updates keeps track of how frequently each center has been updated.
-    n_updates = np.zeros(N_CENTERS)
-    # Online k-means algorithm - stochastic gradient descent.
-    for iteration in range(n_iterations):
-        x = data[iteration % data.shape[0]]
-        min_distance = sys.maxint
-        # For a given datapoint, find the center that is closest to it.
-        for center_index in range(N_CENTERS):
-            distance = np.linalg.norm(x - centers[center_index])
-            if distance < min_distance:
-                min_distance = distance
-                closest_center_index = center_index
-        n_updates[closest_center_index] += 1
-        # Use a learning rate of 1 / #updates(center).
-        centers[closest_center_index] +=  ((x - centers[closest_center_index])
-                * 2 / n_updates[closest_center_index])
-    return centers
+N_CLUSTERS = 200
+N_ITERATIONS = 10
+N_SAMPLES = 10000
 
 def mapper(key, value):
-    # TODO(f-daniel): checkout k-means++ for center updates
-    yield "key", k_means(value, N_PASSES * value.shape[0])
+    yield "key", value
 
 def reducer(key, values):
-    # key: key from mapper used to aggregate
-    # values: list of all value for that key
-    # Note that we do *not* output a (key, values) pair here.
-    yield k_means(values, N_PASSES * values.shape[0])
+    # Size of the dataset.
+    n = values.shape[0]
+    # Dimensionality of the data.
+    d = values.shape[1]
+
+    # Initialize cluster centers with D^2 sampling. This is the same
+    # initialization that is used in k-means++.
+
+    # Array containing the distances from each center to each datapoint.
+    distances = np.full((N_CLUSTERS - 1, n), sys.maxint)
+    centers = np.empty((N_CLUSTERS, d))
+    center = values[randint(n)]
+    centers[0] = center
+    for cluster_index in range(1, N_CLUSTERS):
+        distances[cluster_index - 1] = norm(values - center, axis = 1)**2
+        min_distances = distances.min(axis = 0)
+        p = min_distances / min_distances.sum()
+        center = values[choice(n, p = p)]
+        centers[cluster_index] = center
+
+    # Use a pseudo coreset begin a uniformly sampled subset of the data with
+    # equal weights (1).
+    np.random.shuffle(values)
+    coreset = [(values[i], 1) for i in range(N_SAMPLES)]
+    for iteration in range(N_ITERATIONS):
+        # The sum of points that are closest to a given cluster center.
+        cluster_sum = np.zeros((N_CLUSTERS, d))
+        # The number of points that are closest to a given cluster center.
+        cluster_count = np.zeros(N_CLUSTERS)
+        for point, w in coreset:
+            closest_cluster_index = norm(centers - point, axis = 1).argmin(axis
+                    = 0)
+            cluster_sum[closest_cluster_index] += w * point
+            cluster_count[closest_cluster_index] += w
+        for cluster_index in range(N_CLUSTERS):
+            if cluster_count[cluster_index] > 0:
+                centers[cluster_index] = (cluster_sum[cluster_index] /
+                        cluster_count[cluster_index])
+    yield centers
