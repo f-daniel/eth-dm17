c7ea96b3b0a59b9093c198f198cf40330c1fc15d
kkleindev <kleinkevin.lu@gmail.com>
2017-12-14 15:40:12 +0100

    Use whole dataset and more iterations in kmeans++.

--- 3-k_means/src/runnable.py
+++ 3-k_means/src/runnable.py
@@ -1,15 +1,18 @@
 import numpy as np
 import sys
 
+from sklearn.cluster import MiniBatchKMeans
+
 N_CENTERS = 200
 # The number of times the k-means algorithms passes over the whole dataset at hand.
-N_PASSES = 4
+N_PASSES = 1
 
 def k_means(data, n_iterations):
     # Center initialization: 200 random points from dataset.
     np.random.shuffle(data)
     centers = data[:N_CENTERS,:]
     np.random.shuffle(data)
+
     # n_updates keeps track of how frequently each center has been updated.
     n_updates = np.zeros(N_CENTERS)
     # Online k-means algorithm - stochastic gradient descent.
@@ -31,9 +34,17 @@ def k_means(data, n_iterations):
 def mapper(key, value):
     # TODO(f-daniel): checkout k-means++ for center updates
     yield "key", k_means(value, N_PASSES * value.shape[0])
+    # kmeans = MiniBatchKMeans(init='k-means++', n_clusters=200)
+    # kmeans.fit(value)
+    # centroids = kmeans.cluster_centers_
+    # yield "key", centroids
 
 def reducer(key, values):
     # key: key from mapper used to aggregate
     # values: list of all value for that key
     # Note that we do *not* output a (key, values) pair here.
     yield k_means(values, N_PASSES * values.shape[0])
+    # kmeans = MiniBatchKMeans(init='k-means++', n_clusters=200)
+    # kmeans.fit(values)
+    # centroids = kmeans.cluster_centers_
+    # yield centroids
